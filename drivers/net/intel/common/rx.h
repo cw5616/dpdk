cw5616/dpdk

commit 9eb60580d155f5e3a36927dbb1e59ef9623231ce

net/intel: extract common Rx vector criteria
While some drivers have specific criteria for when a vector driver can
be enabled on the Rx path, there are a number of basic criteria which
apply across all drivers. Centralize those in the intel/common folder,
and then update drivers to use the common conditional checks. This adds
some additional restrictions to some drivers like ixgbe, where those
conditions were necessary but never checked.

Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
Acked-by: Praveen Shetty <praveen.shetty@intel.com>

drivers/net/intel/common/rx.h
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <rte_mbuf.h>
+#include <rte_ethdev.h>
 
 #define CI_RX_BURST 32
 
@@ -87,6 +88,25 @@ ci_rxq_mbuf_initializer(uint16_t port_id)
 	rte_mbuf_refcnt_set(&mb_def, 1);
 
 	return mb_def.rearm_data[0];
+}
+
+/* basic checks for a vector-driver capable queue.
+ * Individual drivers may have other further tests beyond this.
+ */
+static inline bool
+ci_rxq_vec_capable(uint16_t nb_desc, uint16_t rx_free_thresh, uint64_t offloads)
+{
+	if (!rte_is_power_of_2(nb_desc) ||
+			rx_free_thresh < CI_RX_BURST ||
+			(nb_desc % rx_free_thresh) != 0)
+		return false;
+
+	/* no driver supports timestamping or buffer split on vector path */
+	if ((offloads & RTE_ETH_RX_OFFLOAD_TIMESTAMP) ||
+			(offloads & RTE_ETH_RX_OFFLOAD_BUFFER_SPLIT))
+		return false;
+
+	return true;
 }
 
 #endif /* _COMMON_INTEL_RX_H_ */

drivers/net/intel/i40e/i40e_rxtx_vec_common.h
@@ -120,9 +120,6 @@ i40e_rx_vec_dev_conf_condition_check_default(struct rte_eth_dev *dev)
 	struct i40e_adapter *ad =
 		I40E_DEV_PRIVATE_TO_ADAPTER(dev->data->dev_private);
 	struct rte_eth_rxmode *rxmode = &dev->data->dev_conf.rxmode;
-	struct i40e_rx_queue *rxq;
-	uint16_t desc, i;
-	bool first_queue;
 
 	/* no QinQ support */
 	if (rxmode->offloads & RTE_ETH_RX_OFFLOAD_VLAN_EXTEND)
@@ -132,31 +129,13 @@ i40e_rx_vec_dev_conf_condition_check_default(struct rte_eth_dev *dev)
 	 * Vector mode is allowed only when number of Rx queue
 	 * descriptor is power of 2.
 	 */
-	if (!dev->data->dev_started) {
-		first_queue = true;
-		for (i = 0; i < dev->data->nb_rx_queues; i++) {
-			rxq = dev->data->rx_queues[i];
-			if (!rxq)
-				continue;
-			desc = rxq->nb_rx_desc;
-			if (first_queue)
-				ad->rx_vec_allowed =
-					rte_is_power_of_2(desc);
-			else
-				ad->rx_vec_allowed =
-					ad->rx_vec_allowed ?
-					rte_is_power_of_2(desc) :
-					ad->rx_vec_allowed;
-			first_queue = false;
-		}
-	} else {
-		/* Only check the first queue's descriptor number */
-		for (i = 0; i < dev->data->nb_rx_queues; i++) {
-			rxq = dev->data->rx_queues[i];
-			if (!rxq)
-				continue;
-			desc = rxq->nb_rx_desc;
-			ad->rx_vec_allowed = rte_is_power_of_2(desc);
+	ad->rx_vec_allowed = true;
+	for (uint16_t i = 0; i < dev->data->nb_rx_queues; i++) {
+		struct i40e_rx_queue *rxq = dev->data->rx_queues[i];
+		if (!rxq)
+			continue;
+		if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh, rxq->offloads)) {
+			ad->rx_vec_allowed = false;
 			break;
 		}
 	}

drivers/net/intel/iavf/iavf_rxtx.c
@@ -200,19 +200,6 @@ check_tx_thresh(uint16_t nb_desc, uint16_t tx_rs_thresh,
 }
 
 static inline bool
-check_rx_vec_allow(struct iavf_rx_queue *rxq)
-{
-	if (rxq->rx_free_thresh >= IAVF_VPMD_RX_MAX_BURST &&
-	    rxq->nb_rx_desc % rxq->rx_free_thresh == 0) {
-		PMD_INIT_LOG(DEBUG, "Vector Rx can be enabled on this rxq.");
-		return true;
-	}
-
-	PMD_INIT_LOG(DEBUG, "Vector Rx cannot be enabled on this rxq.");
-	return false;
-}
-
-static inline bool
 check_tx_vec_allow(struct ci_tx_queue *txq)
 {
 	if (!(txq->offloads & IAVF_TX_NO_VECTOR_FLAGS) &&
@@ -722,7 +709,7 @@ iavf_dev_rx_queue_setup(struct rte_eth_dev *dev, uint16_t queue_idx,
 		ad->rx_bulk_alloc_allowed = false;
 	}
 
-	if (check_rx_vec_allow(rxq) == false)
+	if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh, rxq->offloads))
 		ad->rx_vec_allowed = false;
 
 #if defined RTE_ARCH_X86 || defined RTE_ARCH_ARM

drivers/net/intel/ice/ice_rxtx_vec_common.h
@@ -149,22 +149,10 @@ ice_rx_vec_queue_default(struct ice_rx_queue *rxq)
 	if (!rxq)
 		return -1;
 
-	if (!rte_is_power_of_2(rxq->nb_rx_desc))
-		return -1;
-
-	if (rxq->rx_free_thresh < ICE_VPMD_RX_BURST)
-		return -1;
-
-	if (rxq->nb_rx_desc % rxq->rx_free_thresh)
+	if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh, rxq->offloads))
 		return -1;
 
 	if (rxq->proto_xtr != PROTO_XTR_NONE)
-		return -1;
-
-	if (rxq->offloads & RTE_ETH_RX_OFFLOAD_TIMESTAMP)
-		return -1;
-
-	if (rxq->offloads & RTE_ETH_RX_OFFLOAD_BUFFER_SPLIT)
 		return -1;
 
 	if (rxq->offloads & ICE_RX_VECTOR_OFFLOAD)

drivers/net/intel/iavf/iavf_rxtx.h
@@ -5,6 +5,7 @@
 #ifndef _IAVF_RXTX_H_
 #define _IAVF_RXTX_H_
 
+#include "../common/rx.h"
 #include "../common/tx.h"
 
 /* In QLEN must be whole number of 32 descriptors. */

drivers/net/intel/ixgbe/ixgbe_rxtx_vec_common.h
@@ -168,6 +168,13 @@ ixgbe_rx_vec_dev_conf_condition_check_default(struct rte_eth_dev *dev)
 	if (fconf->mode != RTE_FDIR_MODE_NONE)
 		return -1;
 
+	for (uint16_t i = 0; i < dev->data->nb_rx_queues; i++) {
+		struct ixgbe_rx_queue *rxq = dev->data->rx_queues[i];
+		if (!rxq)
+			continue;
+		if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh, rxq->offloads))
+			return -1;
+	}
 	return 0;
 #else
 	RTE_SET_USED(dev);

commit 3baa00a5740de6a15caea3379c5de9ea6e8ed20c

net/intel: remove redundant Rx offload check
The common function for detecting whether an rxq could use a vector
rx path would automatically disqualify rx queues that had the
timestamp offload enabled. This was incorrect behaviour because the
iavf driver which uses this common function supports timestamp offload
on its vector paths. Fix this by removing the conditional check for
timestamp offload.

However, the remaining check is redundant because three of the four
drivers that use that function now use the common rx path selection
framework which performs essentially the same validation when ensuring
the requested offloads are available on the rx path to be selected. The
fourth driver does not support buffer split at all, so the rx queue will
never have that offload enabled as initialisation would fail before
getting to the path selection step. Therefore remove the conditional
completely.

Fixes: 9eb60580d155 ("net/intel: extract common Rx vector criteria")
Cc: stable@dpdk.org

Signed-off-by: Ciara Loftus <ciara.loftus@intel.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>

drivers/net/intel/common/rx.h
@@ -228,16 +228,11 @@ ci_rxq_mbuf_initializer(uint16_t port_id)
  * Individual drivers may have other further tests beyond this.
  */
 static inline bool
-ci_rxq_vec_capable(uint16_t nb_desc, uint16_t rx_free_thresh, uint64_t offloads)
+ci_rxq_vec_capable(uint16_t nb_desc, uint16_t rx_free_thresh)
 {
 	if (!rte_is_power_of_2(nb_desc) ||
 			rx_free_thresh < CI_RX_MAX_BURST ||
 			(nb_desc % rx_free_thresh) != 0)
-		return false;
-
-	/* no driver supports timestamping or buffer split on vector path */
-	if ((offloads & RTE_ETH_RX_OFFLOAD_TIMESTAMP) ||
-			(offloads & RTE_ETH_RX_OFFLOAD_BUFFER_SPLIT))
 		return false;
 
 	return true;

drivers/net/intel/i40e/i40e_rxtx_vec_common.h
@@ -62,7 +62,7 @@ i40e_rx_vec_dev_conf_condition_check_default(struct rte_eth_dev *dev)
 		struct ci_rx_queue *rxq = dev->data->rx_queues[i];
 		if (!rxq)
 			continue;
-		if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh, rxq->offloads))
+		if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh))
 			return -1;
 	}
 

drivers/net/intel/iavf/iavf_rxtx.c
@@ -727,7 +727,7 @@ iavf_dev_rx_queue_setup(struct rte_eth_dev *dev, uint16_t queue_idx,
 
 #if defined RTE_ARCH_X86 || defined RTE_ARCH_ARM
 	/* check vector conflict */
-	if (ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh, rxq->offloads) &&
+	if (ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh) &&
 			iavf_rxq_vec_setup(rxq)) {
 		PMD_DRV_LOG(ERR, "Failed vector rx setup.");
 		return -EINVAL;

drivers/net/intel/ice/ice_rxtx_vec_common.h
@@ -78,7 +78,7 @@ ice_rx_vec_queue_default(struct ci_rx_queue *rxq)
 	if (!rxq)
 		return -1;
 
-	if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh, rxq->offloads))
+	if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh))
 		return -1;
 
 	if (rxq->proto_xtr != PROTO_XTR_NONE)

drivers/net/intel/ixgbe/ixgbe_rxtx_vec_common.c
@@ -131,7 +131,7 @@ ixgbe_rx_vec_dev_conf_condition_check(struct rte_eth_dev *dev)
 		struct ci_rx_queue *rxq = dev->data->rx_queues[i];
 		if (!rxq)
 			continue;
-		if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh, rxq->offloads))
+		if (!ci_rxq_vec_capable(rxq->nb_rx_desc, rxq->rx_free_thresh))
 			return -1;
 	}
 	return 0;